cbb94d8546b9489e0984505f6f24def3d1cfafdd
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-01-28 22:00:47 +0330

    feat(model): introduce CartItem entity for shopping cart

---
+++ src/com/mall/model/CartItem.java
@@ -0,0 +1,28 @@
+package com.mall.model;
+
+public class CartItem {
+    private final Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CartItem[product=%s, qty=%d]", product.getId(), quantity);
+    }
+}
