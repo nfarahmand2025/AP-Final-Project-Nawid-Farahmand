5a3ec335898bc1c8826dc389e2058575678b6e82
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-02-03 23:23:31 +0330

    Chore: Commentation

--- src/com/mall/model/User.java
+++ src/com/mall/model/User.java
@@ -1,11 +1,20 @@
 package com.mall.model;
 
+import java.util.Objects;
+
+/**
+ * Abstract base class representing a generic user in the system.
+ * Demonstrates Abstraction and Inheritance.
+ */
 public abstract class User {
     protected String id;
     protected String username;
     protected String password;
     protected UserRole role;
 
+    /**
+     * Defines the types of users allowed in the Shopping Mall system.
+     */
     public enum UserRole {
         ADMIN, CUSTOMER
     }
@@ -17,23 +26,35 @@ public abstract class User {
         this.role = role;
     }
 
-    public String getId()
-    {
+    // Getters
+    public String getId() {
         return id;
     }
 
-    public String getUsername()
-    {
+    public String getUsername() {
         return username;
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return password;
     }
 
-    public UserRole getRole()
-    {
+    public UserRole getRole() {
         return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof User))
+            return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) || Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
     }
 }
