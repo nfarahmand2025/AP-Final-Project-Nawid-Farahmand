9f4702a4212bb67e22a74b9dcf7406933ae18d39
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-02-04 18:05:00 +0330

    Add documentation and comments to CartItem class

--- src/com/mall/model/CartItem.java
+++ src/com/mall/model/CartItem.java
@@ -1,9 +1,17 @@
 package com.mall.model;
 
+/**
+ * Represents a specific product and its quantity within a user's shopping cart.
+ * This class allows the cart to track how many of each item are selected.
+ */
 public class CartItem {
     private final Product product;
     private int quantity;
 
+    /**
+     * @param product  The product being added to the cart.
+     * @param quantity The initial number of units selected.
+     */
     public CartItem(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
@@ -17,6 +25,10 @@ public class CartItem {
         return quantity;
     }
 
+    /**
+     * Updates the quantity of the item in the cart.
+     * Useful for dynamic UI updates when the user increases/decreases count.
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
