db2d77e8beee4c1339bebff1d9f34e998930f855
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-02-04 14:32:06 +0330

    Chore: Commentation

--- src/com/mall/model/Administrator.java
+++ src/com/mall/model/Administrator.java
@@ -1,6 +1,14 @@
 package com.mall.model;
 
+/**
+ * Represents a user with elevated privileges to manage the product catalog.
+ * Admins can add, edit, or remove products from the system.
+ */
 public class Administrator extends User {
+
+    /**
+     * Constructs an Administrator. Role is automatically set to ADMIN.
+     */
     public Administrator(String id, String username, String password) {
         super(id, username, password, UserRole.ADMIN);
     }
