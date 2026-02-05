df49803f227cc79ccfec8164600d1ca9b486e8b5
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-02-06 00:48:50 +0330

    docs(persistence): add JavaDoc comments

--- src/com/mall/persistence/DataStorageInterface.java
+++ src/com/mall/persistence/DataStorageInterface.java
@@ -2,6 +2,15 @@ package com.mall.persistence;
 
 import java.io.IOException;
 
+/**
+ * Interface for persisting and retrieving the application's system state.
+ *
+ * Implementations of this interface handle saving and loading
+ * {@link SystemStateDto} objects to and from a specified file path.
+ *
+ * Different implementations may store data in various formats such as
+ * JSON, XML, or binary files.
+ */
 public interface DataStorageInterface {
     void save(String filePath, SystemStateDto state) throws IOException;
 
