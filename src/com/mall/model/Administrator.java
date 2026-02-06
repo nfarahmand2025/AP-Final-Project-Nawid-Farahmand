b4e91db4b13d3f6abe1659fd922111faa15f7504
Nawid Farahmand <nawid.farahmand@aut.ac.ir>
2026-02-07 01:20:30 +0330

    Adminstrator.java is committed

--- src/com/mall/model/Administrator.java
+++ src/com/mall/model/Administrator.java
@@ -5,11 +5,12 @@ package com.mall.model;
  * Admins can add, edit, or remove products from the system.
  */
 public class Administrator extends User {
-
+    
     /**
      * Constructs an Administrator. Role is automatically set to ADMIN.
      */
     public Administrator(String id, String username, String password) {
+
         super(id, username, password, UserRole.ADMIN);
     }
 }
